7c586f9ffdcdc381136387c5c7c093eac0678782
Gloria Jeyech <gjeyech@gmail.com>
2024-10-04 05:17:35 +0300

    list

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,19 @@
 
 def return_evens(num_list):
     pass
+    evens = [num for num in num_list if num % 2 == 0]
+    return evens 
+
+ 
 
 def make_exclamation(sentence_list):
-    pass
+    exclamations = [sentence + '!' for sentence in sentence_list]
+    return exclamations
+
+
+    
+
+
+
+
+
